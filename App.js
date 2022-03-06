159485d33961c86f2f5cc2d4d614e9627a3c95dd
chriscoomber <Christopher.Coomber@metaswitch.com>
2022-03-06 01:18:07 +0000

    Add text validation

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [number, onChangeNumber] = React.useState("0");
+  const [submittedNumber, onChangeSubmittedNumber] = React.useState("0");
+  const [validationError, onChangeValidationError] = React.useState(null);
+
+  // Validate the input string, checking if it's a number between 0 and 20.
+  // If yes - update state `submittedNumber`.
+  // If no - update state `validationError`.
+  function validateAndSubmit(inputString) {
+    // First convert to integer
+    const inputNumber = Number(inputString);
+
+    if (isNaN(inputNumber)) {
+      onChangeValidationError(`Oops, ${inputString} wasn't a number!`);
+      return;
+    }
+
+    if (inputNumber < 0 || inputNumber > 20) {
+      onChangeValidationError(`Oops, ${inputNumber} is not between 0 and 20!`);
+      return;
+    }
+
+    // Success
+    onChangeValidationError(null);
+    onChangeSubmittedNumber(inputNumber);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Please type a number between 0 and 20</Text>
+      {(validationError === null) || <Text>{validationError}</Text>}
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        keyboardType="numeric"
+        onSubmitEditing={(value) => validateAndSubmit(value.nativeEvent.text)}
+      />
+      <Text>{submittedNumber}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +52,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
---
+++ README.md
@@ -0,0 +1,19 @@
+# How to build
+
+This demo uses *expo* to manage deployment. So, follow these instructions: https://docs.expo.dev/get-started/installation/, including installing the expo app on your mobile device.
+
+On your developer machine, in the console, navigate to the directory containing this README and run:
+
+```
+expo install
+```
+
+This will install the dependencies for the app to your computer (these are specified in package.json).
+
+Now you are ready to run the app. Simply open the expo app in your mobile device, and run on your development machine's console:
+
+```
+expo start
+```
+
+and follow the on-screen instructions to launch on your device, or in your browser.
